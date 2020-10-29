62fe6086e59f5602e62495d236769f1baa7398a3
Thomas Gilbert <tcgilbert94@gmail.com>
2020-10-28 21:54:03 -0400

    [js] add a working text input

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-let query = "cats";
-fetch(`https://www.reddit.com/search.json?nsfw=no&q=${query}`)
-  .then((response) => {
-    return response.json();
-  })
-  .then((redditData) => {
-    const redditPostsArray = redditData.data.children;
-    const picArray = pictureArray(redditPostsArray);
-    // console.log(picArray);
+let query;
+const startButton = document.querySelector('#start-button');
+const userInput = document.querySelector('#text-input-box');
+startButton.addEventListener('click', update);
 
-    const bigImg = document.createElement("img");
-    startImageLoop(bigImg, picArray);
-    
-    bigImg.style.width = "400px";
-    bigImg.style.paddingTop="50px"
-    document.querySelector("#container").appendChild(bigImg);
-  })
-  .catch((err) => console.log(err));
+function update() {
+  query = userInput.value;
+  console.log(query);
+  fetchPictures(query);
+}
+
+function fetchPictures(query) {
+  fetch(`https://www.reddit.com/search.json?nsfw=no&q=${query}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((redditData) => {
+      const redditPostsArray = redditData.data.children;
+      const picArray = pictureArray(redditPostsArray);
+      // console.log(picArray);
+  
+      const bigImg = document.createElement("img");
+      startImageLoop(bigImg, picArray);
+      
+      bigImg.style.width = "400px";
+      bigImg.style.paddingTop="50px"
+      document.querySelector("#container").appendChild(bigImg);
+    })
+    .catch((err) => console.log(err));
+}
 
 function randomPost(post) {
   const randomNum = Math.floor(Math.random() * post.length);
@@ -41,3 +53,4 @@ function startImageLoop(img, array) {
     }
   }, 3000);
 }
+
