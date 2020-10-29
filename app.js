03a1d9e417340c02f0f3537cabaf580d4f985742
Thomas Gilbert <tcgilbert94@gmail.com>
2020-10-28 21:14:14 -0400

    [all] new repository

---
+++ app.js
@@ -0,0 +1,43 @@
+let query = "cats";
+fetch(`https://www.reddit.com/search.json?nsfw=no&q=${query}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((redditData) => {
+    const redditPostsArray = redditData.data.children;
+    const picArray = pictureArray(redditPostsArray);
+    // console.log(picArray);
+
+    const bigImg = document.createElement("img");
+    startImageLoop(bigImg, picArray);
+    
+    bigImg.style.width = "400px";
+    bigImg.style.paddingTop="50px"
+    document.querySelector("#container").appendChild(bigImg);
+  })
+  .catch((err) => console.log(err));
+
+function randomPost(post) {
+  const randomNum = Math.floor(Math.random() * post.length);
+  return post[randomNum];
+}
+
+function pictureArray(postsArray) {
+  const picArray = [];
+  for (let i = 0; i < 10; i++) {
+    const pic = randomPost(postsArray);
+    picArray.push(pic.data.thumbnail);
+  }
+  return picArray;
+}
+
+function startImageLoop(img, array) {
+  let count = 0;
+  setInterval(() => {
+    img.src = array[count];
+    count++;
+    if (count == array.length) {
+      count = 0;
+    }
+  }, 3000);
+}
